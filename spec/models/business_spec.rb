a9dd261922f6ca56777fd75c1227212f311560d1
Graham Powrie <graham@asbp.io>
2013-11-10 14:00:16 -0500

    added requirement for name and description

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -3,7 +3,8 @@ describe Lessonable::Business do
   # let(:klass) { class DummyClass; include ; end }
   subject { Business.new }
   
-  it "is a Class" do
-    expect(subject.foo).to eq true
+  it "has a name and description" do
+    expect(subject.respond_to?(:name)).to eq true
+    expect(subject.respond_to?(:description)).to eq true
   end
 end
