5f70dd55167dcc992171ee165c9f3de7dacad080
Graham Powrie <graham@asbp.io>
2013-11-13 12:07:42 -0500

    added subscription cancellation handling, refactored #subscribe_to

--- lib/lessonable/lib/subscriptions.rb
+++ lib/lessonable/lib/subscriptions.rb
@@ -5,30 +5,64 @@ module Lessonable
     def subscribable?(role)
       ROLE_PLANS[role.to_sym].present?
     end
-    def subscribe_as(role,plan="")
-      if subscribable?(role)
-        customer = Stripe::Customer.retrieve(self.customer_id)
-        customer.update_subscription(:plan => plan)
-      else
-        self.role = role
-        return true
+    def unsubscribe(at_period_end: true)
+      customer = Stripe::Customer.retrieve(self.customer_id)
+      response = customer.cancel_subscription(at_period_end: at_period_end)
+      unless at_period_end
+        self.subscription_id  = nil
+        self.role             = "default"
+        self.save
       end
     end
+    def subscribe_to(plan, new_role=false)
+      new_role = role_by_plan(plan) unless new_role # attempt to find role by plan
+      
+      if new_role and not subscribable?(new_role) # role isn't subscribable, just set it
+        self.role = new_role
+      else
+        customer = Stripe::Customer.retrieve(self.customer_id)
+        response = customer.update_subscription(:plan => plan)
+        
+        new_role ||= "default" unless self.role
+        self.role = new_role if new_role and response.status and response.status.match /active|trialing/
+      end
+      self.save
+    end
+    
     
     module ClassMethods
       def process_subscription_created(event)
-        object = self.find_by_customer_id(event.data.object.customer)
-        object.subscription_id = event.data.object.plan.id if event.data.object.status == "active"
+        stripe_customer = Stripe::Customer.retrieve(event.data.object.customer)
+        if stripe_customer.subscription.status == "active"
+          object = self.find_by_customer_id(event.data.object.customer)
+          object.subscription_id = event.data.object.plan.id
+          object.save
+        end
       end
-      def process_invoice_payment_succeeded(event)
-        object = self.find_by_customer_id(event.data.object.customer)
-        plan = event.data.object.lines.data.find{|datum| datum.keys.include?(:plan)}.plan.id
-        object.role = ROLE_PLANS.select{|k,v| v.include?(plan)}.keys.first.to_s
+      def process_subscription_deleted(event)
+        stripe_customer = Stripe::Customer.retrieve(event.data.object.customer)        
+        if stripe_customer.subscription.status == "canceled"
+          object = self.find_by_customer_id(event.data.object.customer)
+          object.subscription_id  = nil
+          object.role             = "default"
+          object.save
+        end
       end
+      # def process_invoice_payment_succeeded(event)
+      #   object = self.find_by_customer_id(event.data.object.customer)
+      #   plan = event.data.object.lines.data.find{|datum| datum.keys.include?(:plan)}.plan.id
+      #   object.role = ROLE_PLANS.select{|k,v| v.include?(plan)}.keys.first.to_s
+      # end
     end
     
     def self.included(receiver)
       receiver.extend ClassMethods
+    end
+    
+    private 
+    def role_by_plan(plan)
+      role = ROLE_PLANS.select{|k,v| v.include?(plan)}.keys
+      role.present? ? role.first.to_s : false
     end
     
   end
--- spec/lib/subscriptions_spec.rb
+++ spec/lib/subscriptions_spec.rb
@@ -14,6 +14,9 @@ describe Lessonable::Subscriptions do
     attr_accessor :customer_id, :subscription_id, :role
   end
   subject { SubscribableClass.new() }
+  before(:each) do
+    allow(subject).to receive(:save).and_return(true)
+  end
 
   describe "#subscribable?(role)" do
     it "responds true if there are subscriptions for a given role" do
@@ -30,32 +33,112 @@ describe Lessonable::Subscriptions do
   
   context "signing up for a subscription" do
     before(:each) do
-      subject.customer_id = "cus_2viuskgyoax3Bz"
-      allow(subject.class).to receive(:find_by_customer_id).with(subject.customer_id).and_return(subject)
+      setup_customer_with_card
     end
 
     it "changes role instantly for free plan" do
-      subject.subscribe_as("default")
+      subject.subscribe_to(nil, "default")
       expect(subject.role).to eq "default"
     end
     
-    let(:plan) { "student awesomeness" }
-    let(:subscription_success_response) { stripe_subscription_created(subject.customer_id, plan) }
-    it "changes subscription_id on successful subscription" do
-      subject.subscribe_as("student", plan)
-      subject.class.process_subscription_created(subscription_success_response)
-      expect(subject.subscription_id).to eq plan
+    it "changes role on successful subscription" do
+      subject.subscribe_to("The biz plan")
+      expect(subject.role).to eq "business"
     end
     
-    let(:invoice_success_response) { stripe_invoice_payment_succeeded(subject.customer_id, plan) }
-    it "changes role on successful receipt" do
-      subject.class.process_invoice_payment_succeeded(invoice_success_response)
+    it "sets the subscription ID upon successful subscription event" do
+      allow(Stripe::Customer).to receive(:retrieve).with(subject.customer_id).and_return(stripe_customer_object(subscription_status:"active"))
+      event = stripe_subscription_created(subject.customer_id, "student awesomeness")
+      subject.class.process_subscription_created(event)
+    end
+
+    context "subscribing to plan without role" do
+      it "allows subscription, default role given if no other role" do
+        subject.subscribe_to("Plan without role")
+        expect(subject.role).to eq "default"
+      end
+      it "role stays the same if already set" do
+        subject.role = "admin"
+        subject.subscribe_to("Plan without role")
+        expect(subject.role).to eq "admin"
+      end
+    end
+    
+    it "raises error when no such plan available" do
+      expect(lambda{subject.subscribe_to("some bogus plan")}).to raise_error(Stripe::InvalidRequestError, "No such plan: some bogus plan")
+    end
+  end
+  
+  context "signing up for a subscription without valid payment method" do
+    before(:each) do
+      subject.customer_id = stripe_customer_without_card
+    end
+    it "raises error when no payment method available" do
+      expect(lambda{subject.subscribe_to("student awesomeness")}).to raise_error(Stripe::InvalidRequestError, "You must supply a valid card")
+    end
+  end
+  
+  context "downgrading/upgrading subscription" do
+    before(:each) do
+      setup_customer_with_card
+      subject.role = "business"
+    end
+    
+    it "downgrades role based on new subscription" do
+      subject.subscribe_to("student with trial")
       expect(subject.role).to eq "student"
     end
     
-    it "raises error when no subscription present for role" do
-      # lambda{   subject.subscribe_as("student", "no such plan") }.should raise_error(Stripe::InvalidRequestError)
-      expect(lambda{subject.subscribe_as("student", "no such plan")}).to raise_error(Stripe::InvalidRequestError)
+    it "upgrades role based on new subscription" do
+      subject.role = "default"
+      subject.subscribe_to("The biz plan")
+      expect(subject.role).to eq "business"
     end
   end
+  
+  context "automatic cancellation" do
+    before(:each) do
+      setup_customer_with_card
+      subject.role            = "business"
+      subject.subscription_id = "abc123"
+    end
+    
+    it "defaults role and removes subscription_id" do
+      allow(Stripe::Customer).to receive(:retrieve).with(subject.customer_id).and_return(stripe_customer_object(subscription_status: "canceled"))
+      subject.class.process_subscription_deleted stripe_subscription_deleted(subject.customer_id)
+      expect(subject.role).to eq "default"
+      expect(subject.subscription_id).to eq nil
+    end
+  end
+  
+  context "manual cancellation" do
+    before(:each) do
+      setup_customer_with_card
+      subject.subscribe_to("The biz plan")
+      subject.subscription_id = "abc123"
+    end
+    
+    it "cancels at period end by default, so changes nothing immediately" do
+      subject.unsubscribe(at_period_end: true)
+      expect(subject.role).to eq "business"
+      expect(subject.subscription_id).to eq "abc123"
+    end
+    it "defaults role and removes subscription_id" do
+      subject.unsubscribe(at_period_end: false)
+      expect(subject.role).to eq "default"
+      expect(subject.subscription_id).to eq nil
+    end
+  end
+  
+  # let(:invoice_success_response) { stripe_invoice_payment_succeeded(subject.customer_id, "student awesomeness") }
+  # it "changes role on successful invoice" do
+  #   subject.class.process_invoice_payment_succeeded(invoice_success_response)
+  #   expect(subject.role).to eq "student"
+  # end
+
+end
+
+def setup_customer_with_card
+  subject.customer_id  = stripe_customer_with_card
+  allow(subject.class).to receive(:find_by_customer_id).with(subject.customer_id).and_return(subject)
 end
