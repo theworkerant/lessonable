89b158423e14dbb8d2ed46c414225df16dac70dd
Graham Powrie <graham@asbp.io>
2013-11-13 20:24:37 -0500

    added overriddable abilities methods to BusinessAbility

--- lib/lessonable/models/business_ability.rb
+++ lib/lessonable/models/business_ability.rb
@@ -11,7 +11,12 @@ module Lessonable
       @business = business
       roles = ROLES[ROLES.index(user.role_for(@business))..-1]
       roles.each { |role| send(role) }
+      
+      ROLES.each{ |role| self.send :"#{role}_abilities" }
     end
+    
+    # Methods to be overridden
+    ROLES.each {|role| define_method("#{role}_abilities") {} }
     
     def owner
       can :manage, Business, {id: @business.id}
--- spec/lib/business_ability_spec.rb
+++ spec/lib/business_ability_spec.rb
@@ -1,5 +1,7 @@
 require "spec_helper"
 describe Lessonable::BusinessAbility do
+  class FakeClass
+  end
   subject { Lessonable::BusinessAbility.new(create(:user), create(:business)) }
 
   it "default can't do anything" do
@@ -16,6 +18,25 @@ describe Lessonable::BusinessAbility do
     
     it "can do anything" do
       expect(subject.can?(:manage, business)).to eq true
+    end
+  end
+  
+  describe "#xxxx_abilities" do
+    it "should respond to each class + _abilities" do
+      Lessonable::BusinessAbility::ROLES.each do |role|
+        expect(subject.respond_to?("#{role}_abilities")).to eq true
+      end
+    end
+
+    it "can be overridden to provide additional abilities" do
+      expect(subject.can?(:hokey_pokey, FakeClass)).to eq false
+      class Lessonable::BusinessAbility
+        def owner_abilities
+          can :hokey_pokey, FakeClass
+        end
+      end
+      subject = Lessonable::BusinessAbility.new(create(:user), create(:business))
+      expect(subject.can?(:hokey_pokey, FakeClass)).to eq true
     end
   end
   
