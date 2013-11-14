911e0339d35a8418b2d178be3e6dae0ae59dd525
Graham Powrie <graham@asbp.io>
2013-11-13 20:19:39 -0500

    added overriddable ability methods

--- lib/lessonable/models/ability.rb
+++ lib/lessonable/models/ability.rb
@@ -12,20 +12,22 @@ module Lessonable
       user.role ||= "default"
       roles = ROLES[ROLES.index(user.role)..-1]
       roles.each { |role| send(role) }
+      
+      ROLES.each{ |role| self.send :"#{role}_abilities" }
     end
+    
+    # Methods to be overridden
+    ROLES.each {|role| define_method("#{role}_abilities") {} }
     
     def admin
       can :manage, :all
     end
-      
     def business
       can :create, Business
     end
     def instructor
     end
-    
     def student
-      
     end
     def default
     end
--- spec/lib/ability_spec.rb
+++ spec/lib/ability_spec.rb
@@ -1,15 +1,36 @@
 require "spec_helper"
 describe Lessonable::Ability do
+  class FakeClass
+  end
   subject { Lessonable::Ability.new(create(:user)) }
-
+  
   it "default can't do anything" do
-    expect(subject.can?(:do_anything, Class)).to eq false
+    expect(subject.can?(:do_anything, FakeClass)).to eq false
   end
   
   describe "Admin" do
     subject { Lessonable::Ability.new(create(:user, {role: "admin"})) }
     it "can do anything" do
-      expect(subject.can?(:do_anything, Class)).to eq true
+      expect(subject.can?(:do_anything, FakeClass)).to eq true
+    end
+  end
+  
+  describe "#xxxx_abilities" do
+    it "should respond to each class + _abilities" do
+      Lessonable::Ability::ROLES.each do |role|
+        expect(subject.respond_to?("#{role}_abilities")).to eq true
+      end
+    end
+
+    it "can be overridden to provide additional abilities" do
+      expect(subject.can?(:hokey_pokey, FakeClass)).to eq false
+      class Lessonable::Ability
+        def business_abilities
+          can :hokey_pokey, FakeClass
+        end
+      end
+      subject = Lessonable::Ability.new(create(:user))
+      expect(subject.can?(:hokey_pokey, FakeClass)).to eq true
     end
   end
   
